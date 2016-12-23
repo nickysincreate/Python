34f44242a25668bf5fb5d03b83034a42471a0ea5
nickysincreate <nicky.singh2008@gmail.com>
2016-12-23 02:27:45 -0800

    Create Levenshtein_distance.py

---
+++ Levenshtein_distance.py
@@ -0,0 +1,17 @@
+# Calculate Levenshtein distance 2 srings and its case sensitive 
+#Levenshtein distance (LD) is a measure of the similarity between two strings, which we will refer to as the source string (s) and the target string (t). The distance is the number of deletions, insertions, or substitutions required to transform s into t.
+def edit_distance(s1, s2):
+    m=len(s1)+1
+    n=len(s2)+1
+
+    tbl = {}
+    for i in range(m): tbl[i,0]=i
+    for j in range(n): tbl[0,j]=j
+    for i in range(1, m):
+        for j in range(1, n):
+            cost = 0 if s1[i-1] == s2[j-1] else 1
+            tbl[i,j] = min(tbl[i, j-1]+1, tbl[i-1, j]+1, tbl[i-1, j-1]+cost)
+
+    return tbl[i,j]
+
+print(edit_distance("Helloworld", "HalloWorld"))
